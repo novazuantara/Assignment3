c55a54c77d01ce2bc0e6dfa4430b5062a838ebc7
novazuantara <novazuantara7@gmail.com>
2021-11-04 11:22:16 +0700

    Ini tugas Assignmnent 3

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var recipe = ['a','b','c','d'];
+var last_step;
+
+last_step = recipe.length - 1;
+
+console.log(recipe[0]);
+console.log(recipe[last_step]);
---
+++ tugas2.js
@@ -0,0 +1,10 @@
+var my_bucket_list = [];
+// .push() //menambah akhir
+// .unshift() //menambah depan
+// .pop() //menghapus awal
+// .shift() //menghapus akhir
+my_bucket_list.push('roti');
+my_bucket_list.unshift('nasi');
+// my_bucket_list.pop();
+my_bucket_list.shift();
+console.log(my_bucket_list);
